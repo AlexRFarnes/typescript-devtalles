b62ac3f05eeebea35727dbc5bc130ca19f354859
Alex R. Farnes <alexrfarnes@gmail.com>
2026-05-28 11:59:53 +0800

    tipo numeros

---
+++ src/tipos/numbers.ts
@@ -0,0 +1,15 @@
+let avengers: number = 5;
+
+console.log(avengers);
+
+const villains: number = 20;
+
+if (avengers < villains) {
+  console.log("Estamos en problemas");
+} else {
+  console.log("Estamos a salvo");
+}
+
+avengers = 1236;
+
+console.log({ avengers });
