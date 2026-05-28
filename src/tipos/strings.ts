a729459696d49ee7a5a90044892def50020f0a32
Alex R. Farnes <alexrfarnes@gmail.com>
2026-05-28 13:13:44 +0800

    tipo cadenas de caracteres

---
+++ src/tipos/strings.ts
@@ -0,0 +1,9 @@
+const batman: string = "Batman";
+const superman: string = `Superman`;
+
+console.log(batman.toUpperCase());
+
+console.log(batman[0]?.toUpperCase());
+console.log(batman[10]?.toUpperCase());
+console.log(batman.at(-1)?.toUpperCase());
+console.log(batman.at(10)?.toUpperCase());
