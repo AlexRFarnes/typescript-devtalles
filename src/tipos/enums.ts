912cdbc8a6ccc8f9029285f3e48f5651e0f31aed
Alex R. Farnes <alexrfarnes@gmail.com>
2026-05-29 18:08:59 +0800

    tipo enumeraciones

---
+++ src/tipos/enums.ts
@@ -0,0 +1,19 @@
+enum IssueLevel {
+  "critical",
+  "medium",
+  "low",
+}
+
+let newIssue = IssueLevel.critical;
+
+console.log(newIssue);
+
+newIssue = IssueLevel.medium;
+
+console.log(newIssue);
+
+newIssue = IssueLevel.low;
+
+console.log(newIssue);
+
+console.log(IssueLevel);
