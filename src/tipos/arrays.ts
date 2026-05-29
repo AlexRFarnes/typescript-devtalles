e77f49ff88ac31b72f4e30dd74bb4881b3afce5e
Alex R. Farnes <alexrfarnes@gmail.com>
2026-05-29 17:58:57 +0800

    tipo arreglos

---
+++ src/tipos/arrays.ts
@@ -0,0 +1,9 @@
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numbers.push(11);
+
+console.log(numbers);
+
+const names: string[] = ["Juan", "Sofia", "Pedro", "Ana", "Maria", "Jose"];
+
+names.forEach(v => console.log(v.toUpperCase()));
