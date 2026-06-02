7ca3a51267f6fdb3e1c44d002ed056a52aa2f7d9
Alex R. Farnes <alexrfarnes@gmail.com>
2026-06-02 13:04:16 +0800

    funciones

---
+++ src/funciones/functions.ts
@@ -0,0 +1,11 @@
+const hero: string = "Flash";
+
+function returnName(): string {
+  return hero;
+}
+
+const activateBatisignal = (): string => {
+  return "Batiseñal activada";
+};
+
+const heroName = returnName();
